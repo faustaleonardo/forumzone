386b579b6f7b907870dd38b2e3c891ad07093a65
Fausta Leonardo <faustaleonardo11@gmail.com>
2020-02-20 17:27:01 +0700

    added connection to mongodb

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 dotenv.config({
   path: './config.env'
@@ -8,12 +9,36 @@ dotenv.config({
 
 const app = express();
 
+/** MIDDLEWARE */
 app.use(morgan('combined'));
+app.use(express.json());
+/** END: MIDDLEWARE */
 
+/** DATABASE */
+const options = {
+  useNewUrlParser: true,
+  useCreateIndex: true
+};
+
+const uri = process.env.DATABASE.replace(
+  /<password>/,
+  process.env.DATABASE_PASSWORD
+);
+
+try {
+  mongoose.connect(uri, options);
+  console.log('Connected to Database');
+} catch (err) {
+  console.log('Failed connecting to Database');
+}
+/** END: DATABASE */
+
+/** API */
 app.post('/api/v1/signup', (req, res, next) => {
   res.json({
     status: 'success'
   });
 });
+/** END: API */
 
 module.exports = app;
