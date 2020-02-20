07596cb118e90471ccdbf3f481a3bc120adadb79
Fausta Leonardo <faustaleonardo11@gmail.com>
2020-02-20 17:06:03 +0700

    added environment variable configuration

--- server/app.js
+++ server/app.js
@@ -1,5 +1,21 @@
-const app = require('./server');
+const express = require('express');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
 
-app.get('/', (req, res, next) => {
-  res.end('hello');
+dotenv.config({
+  path: './config.env'
 });
+
+const app = express();
+
+app.use(morgan('combined'));
+
+console.log(process.env);
+
+app.post('/api/v1/signup', (req, res, next) => {
+  res.json({
+    status: 'success'
+  });
+});
+
+module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,11 +1,7 @@
-const express = require('express');
+const app = require('./app');
 
-const app = express();
-
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-module.exports = app;
