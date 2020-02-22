1e9c32eed4e2bd8be8d8e702a1fb11d4b3dcb5ed
Fausta Leonardo <faustaleonardo11@gmail.com>
2020-02-22 16:31:18 +0700

    refactor authController

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -6,10 +6,30 @@ const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 const sendEmail = require('../utils/sendEmail');
 
-const sendToken = id => {
+const createToken = id => {
   return jwt.sign({ id }, process.env.JWT_SECRET_KEY, {
     expiresIn: process.env.JWT_EXPIRES_IN
   });
+};
+
+const sendToken = (token, res, statusCode) => {
+  res.status(statusCode).json({
+    status: 'success',
+    token
+  });
+};
+
+const getMessageOptions = (email, url) => {
+  const text = `Forgot your password? Submit a PATCH request with your new password and passwordConfirmation to: ${url}.\nIf you didn't forget your password, please ignore this email!`;
+  const subject = 'Reset Your Password';
+
+  const options = {
+    email,
+    subject,
+    text
+  };
+
+  return options;
 };
 
 exports.signup = catchAsync(async (req, res, next) => {
@@ -22,63 +42,53 @@ exports.signup = catchAsync(async (req, res, next) => {
     passwordConfirmation
   });
 
-  const token = sendToken(newUser._id);
+  const token = createToken(newUser._id);
 
-  res.status(204).json({
-    status: 'success',
-    token
-  });
+  sendToken(token, res, 204);
 });
 
 exports.login = catchAsync(async (req, res, next) => {
   const { email, password } = req.body;
   if (!email || !password)
-    return next(new AppError('Please provide both email and password', 400));
+    next(new AppError('Please provide both email and password', 400));
 
   const user = await User.findOne({ email });
   if (!user || !(await user.verifyPassword(password, user.password)))
-    return next(new AppError('Your email or password is incorrect', 401));
+    next(new AppError('Your email or password is incorrect', 401));
 
-  const token = sendToken(user.id);
-
-  res.status(200).json({
-    status: 'success',
-    token
-  });
+  const token = createToken(user.id);
+  sendToken(token, res, 200);
 });
 
 exports.forgetPassword = catchAsync(async (req, res, next) => {
   const { email } = req.body;
-  if (!email) return next(new AppError('Please provide your email', 400));
+  if (!email) next(new AppError('Please provide your email', 400));
 
   const user = await User.findOne({ email });
   if (!user)
     return next(new AppError('User with that email is not found', 404));
 
   const token = user.createResetToken();
+  // exclude passwordConfirmation requirement
   await user.save({ validateBeforeSave: false });
   const url = `${req.protocol}://${req.get(
     'host'
   )}/api/v1/users/resetPassword/${token}`;
 
   try {
-    const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${url}.\nIf you didn't forget your password, please ignore this email!`;
-    const options = {
-      email: user.email,
-      subject: 'Reset Your Password',
-      text: message
-    };
-
+    const options = getMessageOptions(email, url);
     await sendEmail(options);
+
     res.status(200).json({
       status: 'success',
-      message: 'A reset password url has been sent to your email.'
+      message: 'A reset password token has been sent to your email.'
     });
   } catch (err) {
     user.passwordResetToken = undefined;
     user.passwordResetExpires = undefined;
+
     await user.save({ validateBeforeSave: false });
-    next(new AppError('Something went wrong. Try again later.', 500));
+    next(new AppError('Error when sending email. Try again later.', 500));
   }
 });
 
@@ -86,7 +96,7 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
   const resetToken = req.params.token;
   const { password, passwordConfirmation } = req.body;
 
-  if (!resetToken) return next(new AppError('No reset token is provided', 404));
+  if (!resetToken) next(new AppError('No reset token is provided', 404));
 
   const hashedToken = crypto
     .createHash('sha256')
@@ -98,7 +108,7 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
     passwordResetExpires: { $gt: Date.now() }
   });
 
-  if (!user) return next(new AppError('Token is invalid or expired!', 400));
+  if (!user) next(new AppError('Token is invalid or expired!', 400));
 
   user.password = password;
   user.passwordConfirmation = passwordConfirmation;
@@ -108,12 +118,8 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
 
   await user.save();
 
-  const token = sendToken(user._id);
-
-  res.status(200).json({
-    status: 'success',
-    token
-  });
+  const token = createToken(user._id);
+  sendToken(token, res, 200);
 });
 
 exports.protect = catchAsync(async (req, res, next) => {
@@ -124,7 +130,6 @@ exports.protect = catchAsync(async (req, res, next) => {
   }
 
   const { id } = await promisify(jwt.verify)(token, process.env.JWT_SECRET_KEY);
-
   const user = await User.findById(id);
   req.user = user;
 
